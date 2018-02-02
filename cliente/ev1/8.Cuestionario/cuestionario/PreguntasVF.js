e2f2459c3b2e289d5b1a47014073ccab00e5f3a8
ik012108aoi <34056129+ik012108aoi@users.noreply.github.com>
2018-02-02 18:55:17 +0100

    Casa

---
+++ cliente/ev1/8.Cuestionario/cuestionario/PreguntasVF.js
@@ -0,0 +1,12 @@
+// Javascript Document
+var iPreguntas=4;
+var aPreguntas=[];
+var aRespuestas=[];
+aPreguntas[1]='Paris es la capital de Alemania';
+aRespuestas[1]= 'F';
+aPreguntas[2]='El grillo es un insecto';
+aRespuestas[2]= 'V';
+aPreguntas[3]='Forocoches es un foro de coches';
+aRespuestas[3]= 'F';
+aPreguntas[4]='Trump se ti&ntildee el pelo';
+aRespuestas[4]= 'V';
